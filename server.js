0db503225bc27fef4740562c69ea41cffc8917eb
Matt Bruer <mbruer@gmail.com>
2018-12-16 19:28:45 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + "\n", (err)=>{
+    if(err){
+      console.log('Cannot do it');
+    }
+  });
+  next();
+
+})
+
+hbs.registerHelper('getYear',()=>{
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res)=>{
+  //res.send('<h1>Hello Express!<h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hey there camper!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'Aboot Page',
+
+  });
+})
+
+app.get('/bad', (req, res)=>{
+  res.send({
+    errorMessage: "I can't do it!"
+  });
+})
+
+app.listen(3000, ()=>{
+  console.log('it works')
+});
